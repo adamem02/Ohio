3d04f81ccb87366d0b6eeed5508986233942a540
adamem02 <adamem619@gmail.com>
2024-02-03 19:44:00 -0800

    gitignore

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,34 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- USE DATABASE
+USE ecommerce_db;
+
+-- CREATE TABLES
+CREATE TABLE Category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Product (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+CREATE TABLE Tag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(50)
+);
+
+CREATE TABLE ProductTag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES Product(id),
+  FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
+
